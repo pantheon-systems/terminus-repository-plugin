6927d362ccd9bd7f382d50a1aba876689e597969
Kevin Porras <kevin.porras@pantheon.io>
2023-05-12 07:32:41 -0600

    Cleanup and initial command.

---
+++ src/Commands/IcrSiteCreateCommand.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Pantheon\TerminusIcr\Commands;
+
+use Pantheon\Terminus\Commands\TerminusCommand;
+use Pantheon\TerminusHello\Model\Greeter;
+
+/**
+ * Create a new pantheon site using ICR
+ */
+class IcrSiteCreateCommand extends TerminusCommand
+{
+    /**
+     * Creates a new site.
+     *
+     * @authorize
+     *
+     * @command icr:site:create
+     * @aliases icr:site-create
+     *
+     * @param string $site_name Site name
+     * @param string $label Site label
+     * @param string $upstream_id Upstream name or UUID
+     * @option org Organization name, label, or ID
+     * @option region Specify the service region where the site should be
+     *   created. See documentation for valid regions.
+     *
+     * @usage <site> <label> <upstream> Creates a new site named <site>, human-readably labeled <label>, using code from <upstream>.
+     * @usage <site> <label> <upstream> --org=<org> Creates a new site named <site>, human-readably labeled <label>, using code from <upstream>, associated with <organization>.
+     *
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \Pantheon\Terminus\Exceptions\TerminusException
+     */
+
+    public function create($site_name, $label, $upstream_id, $options = ['org' => null, 'region' => null,])
+    {
+        $this->log()->notice('Creating a new site...');
+        $this->log()->notice("Site name: $site_name");
+    }
+}
---
+++ tests/unit/SampleTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Pantheon\TerminusHello\Model;
+
+use PHPUnit\Framework\TestCase;
+
+class SampleTest extends TestCase
+{
+
+    /**
+     * Do nothing.
+     */
+    public function testGreeter()
+    {
+        $this->assertEquals("nothing", "nothing");
+    }
+}
