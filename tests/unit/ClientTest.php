d4b07125a7f93d3fe41527650dc6c07343ef8a74
Kevin Porras <kevin.porras@pantheon.io>
2023-08-09 16:34:08 -0600

    Update tests.

--- tests/unit/ClientTest.php
+++ tests/unit/ClientTest.php
@@ -15,7 +15,7 @@ class ClientTest extends TestCase
         $request->setConfig(new Config(['papi_protocol' => 'http', 'papi_port' => '8080', 'papi_host' => 'localhost']));
         $client = new Client($request, 1000);
 
-        $expected_uri = 'http://localhost:8080/vcs-auth/v1';
+        $expected_uri = 'http://localhost:8080/vcs/v1';
         $this->assertEquals($expected_uri, $client->getPantheonApiBaseUri());
     }
 
@@ -25,7 +25,7 @@ class ClientTest extends TestCase
         $request->setConfig(new Config(['protocol' => 'http', 'port' => '8080', 'papi_host' => 'localhost']));
         $client = new Client($request, 1000);
 
-        $expected_uri = 'http://localhost:8080/vcs-auth/v1';
+        $expected_uri = 'http://localhost:8080/vcs/v1';
         $this->assertEquals($expected_uri, $client->getPantheonApiBaseUri());
     }
 
@@ -35,7 +35,7 @@ class ClientTest extends TestCase
         $request->setConfig(new Config(['papi_host' => 'localhost']));
         $client = new Client($request, 1000);
 
-        $expected_uri = 'https://localhost:443/vcs-auth/v1';
+        $expected_uri = 'https://localhost:443/vcs/v1';
         $this->assertEquals($expected_uri, $client->getPantheonApiBaseUri());
     }
 }
