079d7d5741e4f6abf204f254f4eb8e2a9b954b53
Kevin Porras <kevin.porras@pantheon.io>
2024-09-23 13:12:09 -0600

    Try to get right envrc file.

--- tests/functional.sh
+++ tests/functional.sh
@@ -2,7 +2,7 @@
 
 # Setup fakes.
 cd tests/fakes && pip3 install -r requirements.txt && chmod +x ./fakes.py && ./fakes.py &
-source .envrc.dist
+source $WORKSPACE/.envrc.dist
 
 # Create site.
 terminus repository:site:create $TERMINUS_SITE_NAME $TERMINUS_SITE_NAME drupal-10-composer-managed --org=$ORG_ID --vcs=github
