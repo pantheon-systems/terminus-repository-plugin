b35e021ae2741eb49fce36d0a03c3f61b3df9dc1
Pratik Kamble <pratik.coderx@gmail.com>
2025-03-04 16:27:54 +0530

    DEVX-4171: Add feature to deploy nodejs site on sta. (#49)
    * DEVX-4171: Add feature to deploy nodejs site on sta.
    
    * DEVX-4171: Adds site type as parameter while creating site record.
    
    * DEVX-4171: Pass preferred platform while creating the site.

--- src/Commands/RepositorySiteCreateCommand.php
+++ src/Commands/RepositorySiteCreateCommand.php
@@ -12,6 +12,7 @@ use Pantheon\TerminusRepository\VcsApi\VcsClientAwareTrait;
 use Pantheon\Terminus\Site\SiteAwareInterface;
 use Pantheon\Terminus\Site\SiteAwareTrait;
 use Pantheon\Terminus\Commands\WorkflowProcessingTrait;
+use Pantheon\Terminus\Models\Site;
 use Pantheon\Terminus\Models\Upstream;
 use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Question\ChoiceQuestion;
@@ -73,11 +74,24 @@ class RepositorySiteCreateCommand extends TerminusCommand implements RequestAwar
             throw new TerminusException('The site name {site_name} is already taken.', compact('site_name'));
         }
 
+        // Make sure this is an actual id as the param could either by id, machine name or label.
+        $upstream_id = $this->getUpstreamId($upstream_id);
+
+        // Locate upstream.
+        $icr_upstream = $this->getIcrUpstream($upstream_id);
+
+        $site_type = $this->getSiteType($icr_upstream);
+
+        // Get preferred platform for the framework.
+        // Right now on nodejs sites is getting deployed on the STA.
+        $preferred_platform = $this->getPreferredPlatformForFramework($site_type);
+
         // Site creation in Pantheon. This code is mostly coming from Terminus site:create command.
         $workflow_options = [
             'label' => $label,
             'site_name' => $site_name,
             'has_external_vcs' => true,
+            'preferred_platform' => $preferred_platform,
         ];
         // If the user specified a region, then include it in the workflow
         // options. We'll allow the API to decide whether the region is valid.
@@ -87,12 +101,6 @@ class RepositorySiteCreateCommand extends TerminusCommand implements RequestAwar
         }
 
         $user = $this->session()->getUser();
-
-        // Make sure this is an actual id as the param could either by id, machine name or label.
-        $upstream_id = $this->getUpstreamId($upstream_id);
-
-        // Locate upstream.
-        $icr_upstream = $this->getIcrUpstream($upstream_id);
 
         // Locate organization.
         $org = $user->getOrganizationMemberships()->get($org_id)->getOrganization();
@@ -112,7 +120,7 @@ class RepositorySiteCreateCommand extends TerminusCommand implements RequestAwar
             'org_uuid' => $workflow_options['organization_id'] ?? $user->id,
             'site_uuid' => $site_uuid,
             'site_name' => $site_name,
-            'site_type' => $this->getSiteType($icr_upstream),
+            'site_type' => $site_type,
         ];
 
         try {
@@ -455,6 +463,8 @@ class RepositorySiteCreateCommand extends TerminusCommand implements RequestAwar
             case 'wordpress':
             case 'wordpress-network':
                 return 'cms-wordpress';
+            case 'nodejs':
+                return 'nodejs';
             default:
                 throw new TerminusException('Framework {framework} not supported.', compact('framework'));
         }
@@ -491,6 +501,8 @@ class RepositorySiteCreateCommand extends TerminusCommand implements RequestAwar
                 return $user->getUpstreams()->get('wordpress-icr');
             case 'wordpress-network':
                 return $user->getUpstreams()->get('wordpress-multisite-icr');
+            case 'nodejs':
+                return $user->getUpstreams()->get('nodejs');
             default:
                 throw new TerminusException('Framework {framework} not supported.', compact('framework'));
         }
@@ -533,5 +545,14 @@ class RepositorySiteCreateCommand extends TerminusCommand implements RequestAwar
         $user = $this->session()->getUser();
         $upstream = $user->getUpstreams()->get($upstream_id);
         return [$upstream->get('repository_url'), $upstream->get('repository_branch')];
+    }
+
+    private function getPreferredPlatformForFramework($framework)
+    {
+        // ATM only nodejs framework is supported on the STA platform update it when more frameworks are supported.
+        if ($framework == 'nodejs') {
+            return 'sta';
+        }
+        return 'cos';
     }
 }
