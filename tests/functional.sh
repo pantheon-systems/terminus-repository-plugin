e889b890908bc7298e1caa4de18418bea3b33d2d
Kevin Porras <kevin.porras@pantheon.io>
2025-05-09 13:23:54 -0600

    CI tests.

--- tests/functional.sh
+++ tests/functional.sh
@@ -12,7 +12,7 @@ terminus repository:site:create $REPOSITORY_SITE_CREATE_SITE_NAME $REPOSITORY_SI
 
 # Pantheon hosted site create.
 echo "Creating Pantheon hosted site..."
-terminus site:create $$PANTHEON_SITE_CREATE_SITE_NAME $PANTHEON_SITE_CREATE_SITE_NAME drupal-11-composer-managed --org="$ORG_ID" --vcs-provider=pantheon
+terminus site:create $PANTHEON_SITE_CREATE_SITE_NAME $PANTHEON_SITE_CREATE_SITE_NAME drupal-11-composer-managed --org="$ORG_ID" --vcs-provider=pantheon
 
 # Github hosted site create.
 echo "Creating Github hosted site..."
